d136f8e6ba820a2849b4663ba16198ac96e637cc
lemonh4733 <simke95159@gmail.com>
2020-03-28 22:27:21 +0200

    Sutvarkyta errorai beveik visi, po daug update neveikia user info ir registracija meta error kai registruojiesi, bet user prideda, sutvarkysiu situos kita savaite.

---
+++ backend/app/Http/Controllers/PortfolioController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Portfolio;
+use App\User;
+use App\Message;
+use App\Role;
+use App\Rating;
+use Illuminate\Http\Request;
+
+class PortfolioController extends Controller
+{
+    public function create(Request $request) {
+        try {
+            $user = auth()->userOrFail();
+        } catch(\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
+        return Portfolio::create([
+            'description' => $request->input('description'),
+            'skills' => $request->input('skills'),
+            'services' => $request->input('services'),
+            'price_per_hour' => $request->input('price_per_hour'),
+            'status' => 0,
+            'user_id' => $request->input('user_id'),
+        ]);
+    }
+    /*User INFO (PO ROLIU UPDATE NEBEVEIKIA.ATNAUJINSIU)  */
+    public function aboutUser(User $user, $id) {
+        $role_id = User::select('role')->where('users.id',$id)->get()[0]->role;
+        $usr = User::select('name', 'email', 'foto', 'location', 'role as group')->join('roles','roles.id','=','group_id')->where('users.id',$id)->get();
+        $portf = Portfolio::select('*')->where('user_id',$id)->get();
+        if ($role_id != 1) {
+            if(!count($portf)) {
+                $portf = ['error' => 'Empty Portfolio'];
+            }
+            $info = [
+                'info' => $usr,
+                'portfolio' => $portf,
+            ];
+        } else {
+            $info = [
+                'info' => $usr,
+            ];
+        }
+        try {
+            $user = auth()->userOrFail();
+        } catch(\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
+        return response()->json($info, 200);
+    }
+    public function update($id, Request $request) {
+        try {
+            $user = auth()->userOrFail();
+        } catch(\Tymon\JWTAuth\Exceptions\UserNotDefinedException $e) {
+            return response()->json(['error' => $e->getMessage()]);
+        }
+        return Portfolio::where('id',$id)->update($request->except(['_token']));
+    }
+}
---
+++ backend/routes/api.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+//Registration/Login
+Route::post('register','ApiController@register');
+Route::post('login','ApiController@login');
+Route::get('new_token', 'ApiController@tokenRefresh');
+
+//User
+Route::post('portfolio', 'PortfolioController@create');
+Route::post('update/portfolio/{id}', 'PortfolioController@update');
+Route::get('user/{id}', 'PortfolioController@aboutUser');
+
+//Roles
+Route::get('role', 'RoleController@aboutRole'); //Roliu sarašas
+Route::get('role/user{id}', 'RoleController@aboutRoleUser'); //Roliu user sarašas pagal user id
+Route::post('add/role', 'RoleController@create');
+
+//Message
+Route::get('message/{id}', 'MessageController@aboutMessage'); //Išsiųstos
+Route::post('message', 'MessageController@create');
+Route::delete('message/delete/{message}', 'MessageController@destroy');
+
+//Rating
+Route::get('all/rating', 'RatingController@aboutRating');
+Route::post('rating', 'RatingController@create');
+Route::delete('rating/delete/{rating}', 'RatingController@destroy');
+
+Route::get('/ro', 'RoleUserController@index');
