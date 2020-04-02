f9ae05464472e152bd1ebf51cccc3211fe226454
Karolis Vasiliauskas <Karolis1919@gmail.com>
2020-04-02 12:39:23 +0300

    Bazinis stilius done
    Bazinis stilius done

--- frontend/admin-side/src/components/App/App.js
+++ frontend/admin-side/src/components/App/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Main from "../Main/Main";
+import Sidebar from "../Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      Admin
+      <Main/>
+      <Sidebar/>
     </div>
   );
 }
