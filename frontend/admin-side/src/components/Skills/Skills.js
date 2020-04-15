956df80bb7d16fc582c755a161e5cea044b2b219
Karolis Vasiliauskas <Karolis1919@gmail.com>
2020-04-15 15:28:24 +0300

    Pridejimas in progress
    Pridejimas in progress

--- frontend/admin-side/src/components/Skills/Skills.js
+++ frontend/admin-side/src/components/Skills/Skills.js
@@ -1,18 +1,34 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './Skills.css';
 
-function Skills(){
-    return(
-        <div className="Category">
-            <div className="main">
-                <div className="main-content">
-                    <div className="container-fluid">
-                        <h1>Įgūdžiai</h1>
+class Skills extends Component{
+    constructor() {
+        super()
+        this.state = {
+            test: []
+        };
+    }
+    componentDidMount(){
+        fetch("http://skelbimai.test/api/")
+            .then(res => res.json())
+            .then(data =>{
+                this.setState({
+                    test:data
+                })
+            })
+    }
+    render(){
+        return(
+            <main>
+                <div className="main">
+                    <div className="main-content">
+                        <div className="container-fluid">
+                            <h1>Įgūdžių pridėjimas</h1>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    );
+            </main>
+        )
+    }
 }
-
 export default Skills;
