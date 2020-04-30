1118f021120aa8b305ceeaf0855bc05394676681
ramunas946 <55541090+ramunas946@users.noreply.github.com>
2020-04-30 16:38:59 +0300

    ProjectApprovalController sutvarkytas

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -77,7 +77,7 @@ Route::post('add/role&id={role_id}', 'AdminController@store'); //Prideti role us
 Route::delete('delete/role&id={role_id}/user&id={user_id}', 'AdminController@deleteRole'); //Ištrinti userui role
 
 //Message
-Route::get('message/{senders_id}/{receivers_id}', 'MessageController@fromMsg'); //Paemus išsiųstas žinutes pasikeičia notivication_read i true(1) 
+Route::get('message/{senders_id}/{receivers_id}', 'MessageController@fromMsg'); //Paemus išsiųstas žinutes pasikeičia notivication_read i true(1)
 Route::post('message{id}', 'MessageController@create'); //Išsiusti žinute
 Route::delete('message/delete/{message}', 'MessageController@destroy');
 
@@ -124,7 +124,7 @@ Route::delete('joboffer/delete/{id}','JobOfferController@destroy'); //Atnaujinti
 
 Route::get('project/{id}', 'ProjectApprovalController@index');//nesutvarkytas
 Route::post('project','ProjectApprovalController@create');
-Route::put('project/{project}','ProjectApprovalController@update');
+Route::post('project/{project}','ProjectApprovalController@update');
 Route::delete('project/{id}','ProjectApprovalController@delete');
 
 //Contact form
